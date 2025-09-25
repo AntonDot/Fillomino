f7c94be242312427f1ae24143d8fae1d7137dadd
AntonDot <anton.dotsenko2006@gmail.com>
2025-09-26 01:26:11 +0500

    пошел жарить креветки, визуал и тесты пиши сам

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from HexagonalFillomino import HexagonalFillomino
 from sys import setrecursionlimit
 
 setrecursionlimit(1000000)
-board = {(0, 0): 3, (1, 0): 2, (0, 1): 0, (-1, 1): 0, (-1, 0): 0, (0, -1): 0, (1, -1): 0}
+board = {(0, 0): 3, (1, 0): 2, (0, 1): 0, (-1, 1): 0, (-1, 0): 0, (0, -1): 0, (1, -1): 0} #для этого примера работает!!!
 solver = HexagonalFillomino()
 results = solver.find_solves(board)
 results_str = []
